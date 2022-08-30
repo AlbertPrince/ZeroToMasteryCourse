0b913a05a4074bd68b2f9ffd2a237a7bcb2be7fc
AlbertPrince <apmensah@gmail.com>
2022-08-30 15:18:28 +0000

    math functions

---
+++ main.py
@@ -0,0 +1,14 @@
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    # print(type(2 + 4))
+    # print(2 - 4)
+    # print(2 * 4)
+    # print(2 / 4)
+    print(round(2.5))
+    print(abs(-23))
+    print(20 - 3 * 4)
+    print(int('0b1010101', 2))
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
