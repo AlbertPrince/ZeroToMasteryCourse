1dee2c527a33ca7f32b4316f2b3de4b5a35ccd6f
AlbertPrince <apmensah@gmail.com>
2022-09-17 15:09:56 +0000

    dictionaries

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
+selfish = '01234567'
 
+user = {
+    'basket': [1, 2, 3],
+    'greet': 'hello'
+}
+user2 = dict(name='hello')
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
@@ -6,9 +12,17 @@ if __name__ == '__main__':
     # print(2 - 4)
     # print(2 * 4)
     # print(2 / 4)
-    print(round(2.5))
-    print(abs(-23))
-    print(20 - 3 * 4)
-    print(int('0b1010101', 2))
+    # print(round(2.5))
+    # print(abs(-23))
+    # print(20 - 3 * 4)
+    # print(int('0b1010101', 2))
+    # print(complex("3i + 4j"))
+    # print(selfish[3::2])
+    # print(bool(0))
+    # relationship_status = "hello"
+    # relationship_status = "goodbye"
+    # print(relationship_status)
+    # print(user.get('greet'))
+    print(user2.get('name'))
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
